a8a8f38d8676ed7518a260b690513349b96eb9f4
Joey Yang <joeyyang.ai@gmail.com>
2022-10-26 03:03:09 -0400

    Initial CircularBuffer class

--- test.cpp
+++ test.cpp
@@ -1,31 +1,50 @@
+#include <array>
 #include <iostream>
-#include <zmq_addon.hpp>
+#include <memory>
+
+constexpr size_t BUFFER_SIZE{10}; 
+
+template <typename T, size_t N>
+class CircularBuffer
+{
+public:
+
+    void addElement(T element, unsigned int index)
+    {
+        if ()
+        buffer_[000] = element;
+    }
+
+    template <typename TFriend, size_t NFriend>
+    friend void printBuffer(const CircularBuffer<TFriend, NFriend>& circular_buffer);
+
+private:
+    size_t buffer_size_{N};
+    std::array<T, N> buffer_{};
+};
+
+template <typename TFriend, size_t NFriend>
+void printBuffer(const CircularBuffer<TFriend, NFriend>& circular_buffer)
+{
+    std::cout << "Buffer size: " << circular_buffer.buffer_size_ << "\n";
+
+    for (const auto& element : circular_buffer.buffer_)
+    {
+        std::cout << element << "\n";
+    }
+}
 
 int main()
 {
-    zmq::context_t ctx;
-    zmq::socket_t sock1(ctx, zmq::socket_type::push);
-    zmq::socket_t sock2(ctx, zmq::socket_type::pull);
-    sock1.bind("tcp://127.0.0.1:*");
-    const std::string last_endpoint =
-        sock1.get(zmq::sockopt::last_endpoint);
-    std::cout << "Connecting to "
-              << last_endpoint << std::endl;
-    sock2.connect(last_endpoint);
+    CircularBuffer<int, BUFFER_SIZE> circular_buffer;
 
-    std::array<zmq::const_buffer, 2> send_msgs = {
-        zmq::str_buffer("foo"),
-        zmq::str_buffer("bar!")
-    };
-    if (!zmq::send_multipart(sock1, send_msgs))
-        return 1;
+    printBuffer(circular_buffer);
 
-    std::vector<zmq::message_t> recv_msgs;
-    const auto ret = zmq::recv_multipart(
-        sock2, std::back_inserter(recv_msgs));
-    if (!ret)
-        return 1;
-    std::cout << "Got " << *ret
-              << " messages" << std::endl;
+    circular_buffer.addElement(-9, 1);
+    circular_buffer.addElement(2, 5);
+    circular_buffer.addElement(5, 15);
+
+    printBuffer(circular_buffer);
+
     return 0;
 }
