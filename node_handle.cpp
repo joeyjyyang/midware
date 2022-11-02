c6fe132da24df04ab41e083a3700e7a5349523c2
Joey Yang <joeyyang.ai@gmail.com>
2022-11-02 09:48:35 -0400

    Changes

--- node_handle.cpp
+++ node_handle.cpp
@@ -8,15 +8,56 @@
 class NodeHandle
 {
 public:
+    // Alias for callback function object/functor type.
+    using CallbackType = std::function<TCallbackReturnType(std::string)>;
     NodeHandle() = default;
+
+    Publisher createPublisher(const std::string endpoint)
+    {
+        return Publisher(endpoint);
+    }
+
+    Subscriber createSubscriber(const std::string endpoint, const CallbackType& callback)
+    {
+        return Subscriber(endpoint, callback);
+    }
+
+    Server createServer(const std::string endpoint, const CallbackType& callback)
+    {
+        return Server(endpoint, callback);
+    }
+
+    Client createClient(const std::string endpoint)
+    {
+        return Client(endpoint);
+    }
 
 private:
     zmq::context_t context_{};
 };
 
+std::string testCallback(std::string message)
+{
+    // Do some work.
+
+    std::cout << "Processing message from publisher: " << message << "...\n";
+    const std::string reply = "Processed " + message;
+
+    return reply;
+}
+
 int main(int argc, char* argv[])
 {
     NodeHandle node_handle;
+
+    Subscriber<std::string> subscriber("tcp://localhost:5556", testCallback);
+
+    Publisher publisher = node_handle.createPublisher("tcp://*:5556");
+    
+    while (true)
+    {
+        publisher.publishMessage("test");
+    }
 
     return 0;
 }
