a197f2f2004fd9a5e37ec3fe3e5d062de82e7285
Joey Yang <joeyyang.ai@gmail.com>
2022-11-03 08:56:02 -0400

    Copy constructor and copy assignment operator for unique_ptr

--- matrix.cpp
+++ matrix.cpp
@@ -1,43 +1,46 @@
 #include <cassert>
 #include <iostream>
+#include <memory>
 #include <vector>
 
 class Matrix
 {
 public:
-    Matrix() : num_rows_(0), num_cols_(0), matrix_(new std::vector<std::vector<double>>(num_rows_, std::vector<double>(num_cols_, 0)))
+    using Matrix_t = std::vector<std::vector<double>>;
+
+    // Default constructor.
+    Matrix() : num_rows_(0), num_cols_(0), matrix_(std::make_unique<Matrix_t>(num_rows_, std::vector<double>(num_cols_, 0)))
     {}
 
-    Matrix(const size_t num_rows, const size_t num_cols, const double initial_value) : num_rows_(num_rows), num_cols_(num_cols), matrix_(new std::vector<std::vector<double>>(num_rows_, std::vector<double>(num_cols_, initial_value))) {}
+    // Default constructor.
+    Matrix(const size_t num_rows, const size_t num_cols, const double initial_value) : num_rows_(num_rows), num_cols_(num_cols), matrix_(std::make_unique<Matrix_t>(num_rows_, std::vector<double>(num_cols_, initial_value))) {}
 
-    Matrix(const Matrix& other) : num_rows_(other.num_rows_), num_cols_(other.num_cols_), matrix_(new std::vector<std::vector<double>>(*other.matrix_))
+    // Copy constructor
+    Matrix(const Matrix& other) : num_rows_(other.num_rows_), num_cols_(other.num_cols_), matrix_(std::make_unique<Matrix_t>(*other.matrix_))
     {}
 
+    // Copy assignment operator.
     Matrix& operator=(const Matrix& other)
     {
         if (this != &other)
         {
-            delete matrix_;
-            matrix_ = nullptr;
             num_rows_ = other.num_rows_;
             num_cols_ = other.num_cols_;
-            matrix_ = new std::vector<std::vector<double>>(*other.matrix_);
+            matrix_ = std::make_unique<Matrix_t>(*other.matrix_);
         }
 
-        return *this;
+        return *this; 
     }
 
-    ~Matrix()
+    /*~Matrix()
     {
-        delete matrix_;
+        //delete matrix_;
         matrix_ = nullptr;
     }
 
     Matrix operator*=(Matrix& other)
     {
         assert(num_cols_ == other.num_rows_);
-
-
 
         Matrix product;
 
@@ -52,10 +55,12 @@ public:
     Matrix& crossProduct(Matrix& other)
     {
         
-    }
+    }*/
 
     void print() const
     {
+        std::cout << "Printing matrix: \n";
+
         for (size_t r_i = 0; r_i < num_rows_; r_i++)
         {
             for (size_t c_i = 0; c_i < num_cols_; c_i++)
@@ -70,7 +75,7 @@ public:
 private:
     size_t num_rows_;
     size_t num_cols_;
-    std::vector<std::vector<double>>* matrix_;
+    std::unique_ptr<Matrix_t> matrix_;
 };
 
 int main(int argc, char* argv[])
@@ -82,11 +87,13 @@ int main(int argc, char* argv[])
     matrix_b.print();
 
     Matrix matrix_c;
+    matrix_c.print();
     matrix_c = matrix_a;
+    matrix_c.print();
 
-    Matrix matrix_d(2, 4, 3);
+    /*Matrix matrix_d(2, 4, 3);
     matrix_a *= matrix_d;
-    matrix_a.print();
+    matrix_a.print();*/
 
     return 0;
 }
