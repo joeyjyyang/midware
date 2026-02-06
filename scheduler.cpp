6a1c136ee633d55b8d1aa3cbead1116656c1e0d6
Joey Yang <joeyyang.ai@gmail.com>
2026-02-06 12:45:22 -0500

    Update

--- scheduler.cpp
+++ scheduler.cpp
@@ -18,7 +18,7 @@ public:
     Scheduler& operator=(Scheduler&& other) = delete;
 
     ~Scheduler() {
-        running_.store(false);
+        scheduler_running_.store(false);
 
         for (std::thread& thread : worker_thread_pool_) {
             if (thread.joinable()) {
@@ -31,14 +31,19 @@ public:
         std::lock_guard<std::mutex> lock(schedule_mtx_);
 
         const unsigned int id = ++task_id_;
-        schedule_[id] = {task, freq_hz};
+        task_id_func_hash_table_[id] = {task, freq_hz};
+        task_id_running_hash_table[id] = true;
 
-        worker_thread_pool_.emplace_back([this, id]() {
-            auto [func, freq] = schedule_[id];
-            auto period = std::chrono::duration<double>(1.0 / freq);
+        // Important: Capture copies of task and freq_hz to ensure they remain valid in the worker thread,
+        // since we want to avoid the alternative of using locks inside the worker thread's lambda.
+        auto task_copy = task;
+        auto freq_copy = freq_hz;
 
-            while (running_.load()) {
-                func();
+        worker_thread_pool_.emplace_back([this, id, task_copy, freq_copy]() {
+            auto period = std::chrono::duration<double>(1.0 / freq_copy);
+
+            while (scheduler_running_.load() && isTaskRunning(id)) {
+                task_copy();
                 std::this_thread::sleep_for(period);
             }
         });
@@ -49,17 +54,28 @@ public:
     std::optional<int> deschedule(const unsigned int id) {
         std::lock_guard<std::mutex> lock(schedule_mtx_);
 
-        if (schedule_.erase(id) > 0) {
+        if (task_id_func_hash_table_.erase(id) > 0) {
+            task_id_running_hash_table[id] = false;
+
             return id;
         }
 
         return std::nullopt;
     }
 
+    bool isTaskRunning(const unsigned int id) {
+        std::lock_guard<std::mutex> lock(schedule_mtx_);
+        auto it = task_id_running_hash_table.find(id);
+
+        return it != task_id_running_hash_table.end() && it->second;
+    }
+
 private:
-    std::atomic<bool> running_{true};
+
+    std::atomic<bool> scheduler_running_{true};
     unsigned int task_id_{0};
-    std::unordered_map<int, std::pair<std::function<void()>, double>> schedule_;
+    std::unordered_map<int, std::pair<std::function<void()>, double>> task_id_func_hash_table_;
+    std::unordered_map<int, bool> task_id_running_hash_table;
     std::mutex schedule_mtx_;
     std::vector<std::thread> worker_thread_pool_;
 };
