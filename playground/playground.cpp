5245e50b6c9c94d734143c965e08109c2548f629
Joey Yang <your.email@example.com>
2026-03-13 01:44:51 -0400

    Update

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -9,16 +9,22 @@
 - Performance profiling and optimization
 - C++20 features (concepts, ranges, coroutines)
 - C++17 features (structured bindings, if constexpr, fold expressions)
+- std::optional
 
 */
 
 #include <atomic>
 #include <chrono>
+#include <condition_variable>
 #include <cstdint>
+#include <functional>
 #include <iostream>
 #include <memory>
+#include <mutex>
+#include <queue>
 #include <string>
 #include <thread>
+#include <vector>
 
 class IRuntimeNode {
 public:
@@ -28,6 +34,7 @@ public:
     // Pure virtual function(s) below.
 
     virtual void execute() = 0;
+    virtual double getFrequency() const = 0;
 
 private:
     // Technically, can have static members in interfaces, but considered bad design as interfaces define behavior not state.
@@ -42,7 +49,7 @@ public:
     // Pure virtual function(s) below.
 
     virtual void read() = 0;
-    virtual uint32_t getUUID() const = 0;
+    virtual uint32_t getUuid() const = 0;
 
 private:
     // Technically, can have static members in interfaces, but considered bad design as interfaces define behavior not state.
@@ -51,10 +58,7 @@ private:
 
 class LidarNode : public IRuntimeNode {
 public:
-    // Mark single argument constructors as explicit to prevent unintended implicit conversions.
-    explicit LidarNode(std::unique_ptr<IDriver>&& driver) : driver_(std::move(driver)) {
-        running_.store(true);
-    }
+    LidarNode(std::unique_ptr<IDriver>&& driver, const double frequency) : driver_(std::move(driver)), frequency_(frequency) {}
 
     // Technically, unnecessary since compiler will generate this.
     ~LidarNode() override = default;
@@ -81,25 +85,23 @@ public:
         return *this;
     }
 
-    void stop() {
-        running_.store(false);
+    void execute() final {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        driver_->read();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds\n";
     }
 
-    void execute() final {
-        while (running_.load()) {
-            auto start = std::chrono::high_resolution_clock::now();
-
-            driver_->read();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-
-            auto end = std::chrono::high_resolution_clock::now();
-            std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds\n";
-        }
+    double getFrequency() const final {
+        return frequency_;
     }
 
 private:
+    double frequency_{0.0};
     std::unique_ptr<IDriver> driver_;
-    std::atomic<bool> running_{false};
 };
 
 class LidarDriver : public IDriver {
@@ -130,7 +132,7 @@ public:
         std::cout << "Reading data from LidarDriver with UUID: " << uuid_ << "\n";
     }
 
-    uint32_t getUUID() const final {
+    uint32_t getUuid() const final {
         return uuid_;
     }
 
@@ -139,15 +141,101 @@ private:
 };
 
 class RuntimeGraph {
+public:
+    // Mark single argument constructors as explicit to prevent unintended implicit conversions.
+    explicit RuntimeGraph(const unsigned int num_threads) : num_threads_(num_threads) {
+        running_.store(true);
 
+        for (unsigned int i = 0; i < num_threads_; ++i) {
+            // More efficient than thread_pool_.emplace_back(std::thread([this]() { ... })) since it constructs the thread in place after forwarding the lambda to the thread constructor, avoiding an extra move/copy of the thread object.
+            thread_pool_.emplace_back([this]() {
+                while (true) {
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(mtx_);
+
+                        cv_.wait(lock, [this]() {
+                            return !task_queue_.empty();
+                        });
+
+                        task = std::move(task_queue_.front());
+                        task_queue_.pop();
+                    }
+
+                    // Execute the task outside of the lock to allow other threads to access the queue.
+                    task();
+                }
+            });
+        }
+    }
+
+    ~RuntimeGraph() {
+        return;
+    }
+
+    void registerNode(std::unique_ptr<IRuntimeNode>&& node) {
+        nodes_.emplace_back(std::move(node));
+    }
+
+    void dispatchTask(std::function<void()>&& task) {
+        {
+            std::lock_guard<std::mutex> lock(mtx_);
+            task_queue_.emplace(std::move(task));
+        }
+
+        cv_.notify_one();
+    }
+
+    void run() {
+        for (const auto& node : nodes_) {
+            dispatchTask([&node]() {
+                while(true) {
+                    auto start = std::chrono::high_resolution_clock::now();
+
+                    node->execute();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<int>(1000.0 / node->getFrequency())));
+
+                    auto end = std::chrono::high_resolution_clock::now();
+                    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << " microseconds\n";
+                }
+            });
+        }
+    }
+
+    // // Copy Constructor.
+    // // Technically, unnecessary since compiler will generate this.
+    // RuntimeGraph(const RuntimeGraph&) = default;
+
+    // // Copy Assignment Operator.
+    // // Technically, unnecessary since compiler will generate this.
+    // RuntimeGraph& operator=(const RuntimeGraph&) = default;
+
+    // // Move Constructor.
+    // // Technically, unnecessary since compiler will generate this.
+    // RuntimeGraph(RuntimeGraph&&) noexcept = default;
+
+    // // Move Assignment Operator.
+    // // Technically, unnecessary since compiler will generate this.
+    // RuntimeGraph& operator=(RuntimeGraph&&) noexcept = default;
+
+private:
+    unsigned int num_threads_;
+    std::mutex mtx_;
+    std::condition_variable cv_;
+    std::vector<std::thread> thread_pool_;
+    std::queue<std::function<void()>> task_queue_;
+    std::vector<std::unique_ptr<IRuntimeNode>> nodes_;
+    std::atomic<bool> running_{false};
 };
 
 int main (int argc, char* argv[]) {
-    std::unique_ptr<IDriver> driver = std::make_unique<LidarDriver>(12345);
-    std::unique_ptr<IRuntimeNode> node = std::make_unique<LidarNode>(std::move(driver));
-    // driver is nullptr after move, so cannot be used directly hereafter.
-
-    node->execute();
+    RuntimeGraph runtime_graph(std::thread::hardware_concurrency());
+    std::unique_ptr<IDriver> lidar_driver = std::make_unique<LidarDriver>(12345);
+    std::unique_ptr<IRuntimeNode> lidar_node = std::make_unique<LidarNode>(std::move(lidar_driver), 10.0);
+    // lidar_driver is nullptr after move, so cannot be used directly hereafter.
+    runtime_graph.registerNode(std::move(lidar_node));
+    // lidar_node is nullptr after move, so cannot be used directly hereafter.
 
     return 0;
 }
